279d98ae646cc7d482733926ffaefa131d9d2be5
Molly Nicholas <molecule@berkeley.edu>
2019-03-06 10:57:10 -0800

    refactor. see andriod readme for notes

--- app/src/main/java/com/example/peng/nfcreadwrite/MainActivity.java
+++ app/src/main/java/com/example/peng/nfcreadwrite/MainActivity.java
@@ -50,7 +50,8 @@ public class MainActivity extends Activity {
     Tag myTag;
     Activity context;
 
-    TextView tvNFCContent;
+    TextView prevContent;
+    TextView justRead;
     private TextView justWritten = null;
     private TextView timeToWrite = null;
     TextView message;
@@ -64,7 +65,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         context = this;
 
-        tvNFCContent = (TextView) findViewById(R.id.nfc_contents);
+        prevContent = (TextView) findViewById(R.id.previous_content);
+        justRead = (TextView) findViewById(R.id.just_read);
         timeToWrite = (TextView) findViewById(R.id.time_to_write);
         justWritten = (TextView) findViewById(R.id.just_written);
         message = (TextView) findViewById(R.id.edit_message);
@@ -107,40 +109,78 @@ public class MainActivity extends Activity {
         }
 
         pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
-        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED); // todo: NDEF discovered and tech discovered.
         tagDetected.addCategory(Intent.CATEGORY_DEFAULT);
         writeTagFilters = new IntentFilter[] { tagDetected };
     }
 
     @Override
     public void onNewIntent (final Intent intent) {
-        Log.d( TAG, "onNewIntent");
-        readFromIntent(intent);
-
-        //setIntent(intent);// was in original codexpedia code...need to figure out what it does. //todo
-        //This might be useful: http://www.codexpedia.com/android/android-nfc-read-and-write-example/
-        // this too: https://www.survivingwithandroid.com/2016/01/nfc-tag-writer-android.html
-
-
-        if (intent == null) return;
-        myTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-        Log.d(TAG, "myTag: " + myTag.toString());
-
-        // next chunk mostly from here: https://www.survivingwithandroid.com/2015/03/android-nfc-app-android-nfc-tutorial.html
+        Log.d(TAG, "onNewIntent");
         String type = intent.getType();
         String action = intent.getAction();
         Log.d(TAG, "type: " + type + ", action: " + action);
 
-        NdefRecord[] records;
-        try {
-            records = new NdefRecord[]{ createRecord(message.getText().toString()) };
+        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
+                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
+            if (message.getText().toString().equals("")) {
+                // no message to write, so read only
+                readFromIntent(intent);
+            }
 
-            NdefMessage message = new NdefMessage(records);
-            // Get an instance of Ndef for the tag.
-            Ndef ndef = Ndef.get(myTag);
+            if (intent == null) return;
+
+            // write
+            myTag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            Log.d(TAG, "myTag: " + myTag.toString());
+
+            // build message to write
+            NdefRecord[] records;
+            try {
+                records = new NdefRecord[]{ createRecord(message.getText().toString()) };
+                NdefMessage ndefMsg = new NdefMessage(records);
+
+                // Try to get an instance of Ndef for the tag.
+                Ndef ndef = Ndef.get(myTag);
+                long timeNdefWrite = 100000000;
+                if (ndef != null) {
+                    timeNdefWrite = handleNDEF(ndef, ndefMsg);
+                } else {
+                    MifareUltralight mfu = MifareUltralight.get(myTag);
+                    if (mfu != null) {
+                        // write to mifare
+                        handleMifare();
+                    } else {
+                        String msg = "The Tag could not be identified or this NFC device does not "
+                                + "support the NFC Forum commands needed to access this tag";
+                        String title = "Communication failed";
+                        showAlert(msg, title);
+                    }
+                }
+                if (dialog != null) {
+                    dialog.dismiss();
+                    Toast.makeText(context, "time to write: " + timeNdefWrite, Toast.LENGTH_SHORT).show();
+                    timeToWrite.setText("time to write: " + timeNdefWrite + "ms");
+                    NdefMessage[] msgs = new NdefMessage[1];
+                    msgs[0] = ndefMsg;
+                    buildTagViews(msgs, "Just Written: ", justWritten);; // is this going to byte me in teh butt later?
+                    // justWritten.setText("Successfully wrote: " + new String(message.getRecords()[0].getPayload(), "UTF-8"));
+                }
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+
+        super.onNewIntent( intent );
+    }
+
+    private long handleNDEF(Ndef ndef, NdefMessage ndefMsg) {
+        long timeNdefWrite = 0;
+        try {
 
             // Time statistics to return
-            long timeNdefWrite = 0;
+
             long RegTimeOutStart = System.currentTimeMillis();
             if (ndef != null) {
                 // Enable I/O
@@ -149,70 +189,73 @@ public class MainActivity extends Activity {
                 // read first?
                 NdefMessage readMessage = ndef.getCachedNdefMessage();
                 NdefMessage[] msgs = {readMessage};
-                buildTagViews(msgs, "Just read NFC Content: ", tvNFCContent);
+                buildTagViews(msgs, "Just read NFC Content: ", justRead);
                 //Log.d(TAG, "reading cached msg from ndef tag: " + readMessage.getByteArrayLength());
                 // Write the message
-                Log.d(TAG, "writing message...");
+                Log.d(TAG, "writing message..." + message.toString());
 
                 // Time statistics to return
                 timeNdefWrite = 0;
                 RegTimeOutStart = System.currentTimeMillis();
-                ndef.writeNdefMessage(message);
+                ndef.writeNdefMessage(ndefMsg);
                 Log.d(TAG, "closing connection to tag...");
                 ndef.close();
                 timeNdefWrite = System.currentTimeMillis() - RegTimeOutStart;
-            } else {
-                byte[] answer;
-                byte[] command;
-                MifareUltralight mfu = MifareUltralight.get(myTag);
-                if (mfu != null) {
-                    mfu.connect();
-
-                    // various attempts at reading the tag, all of which fail w/ the default tag and the resined one.
-                    // the one w/ wires coming off works fine.
-                    // This is the example from the android docs and it just prints nonsense:
-                    // �Ten1���l yo
-                    //readTag(); // android version
-                    // read first?
-                    //byte[] pages = readMifareUltralight(mfu); // another website version
-                    // the above one prints out nonsense and more: mfu, pageString: �Ten1���l your cleverness and buy bewilderme
-                    //String pageString = new String(pages);
-                    //Log.d(TAG, "mfu, pageString: " + pageString);
-
-                    //NdefMessage readMessage =
-                    command = new byte[1];
-                    command[0] = (byte) 0x60; // GET_VERSION
-                    answer = mfu.transceive(command);
-                    Log.d(TAG, "answer: " + answer);
-                    mfu.close();
-                } else {
-                    String msg = "The Tag could not be identified or this NFC device does not "
-                            + "support the NFC Forum commands needed to access this tag";
-                    String title = "Communication failed";
-                    showAlert(msg, title);
-                }
+                return timeNdefWrite;
             }
 
-            if (dialog != null) {
-                dialog.dismiss();
-                Toast.makeText(context, "time to write: " + timeNdefWrite, Toast.LENGTH_SHORT).show();
-                timeToWrite.setText("time to write: " + timeNdefWrite + "ms");
-                NdefMessage[] msgs = new NdefMessage[1];
-                msgs[0] = message;
-                buildTagViews(msgs, "Just Written: ", justWritten);; // is this going to byte me in teh butt later?
-                // justWritten.setText("Successfully wrote: " + new String(message.getRecords()[0].getPayload(), "UTF-8"));
-            }
-
-
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
         } catch (FormatException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        super.onNewIntent( intent );
+        return timeNdefWrite;
     }
+
+    // looks like read only????????
+    private void handleMifare() {
+        byte[] answer;
+        byte[] command;
+        MifareUltralight mfu = MifareUltralight.get(myTag);
+        if (mfu != null) {
+            try {
+                mfu.connect();
+                // various attempts at reading the tag, all of which fail w/ the default tag and the resined one.
+                // the one w/ wires coming off works fine.
+                // This is the example from the android docs and it just prints nonsense:
+                // �Ten1���l yo
+                //readTag(); // android version
+                // read first?
+                //byte[] pages = readMifareUltralight(mfu); // another website version
+                // the above one prints out nonsense and more: mfu, pageString: �Ten1���l your cleverness and buy bewilderme
+                //String pageString = new String(pages);
+                //Log.d(TAG, "mfu, pageString: " + pageString);
+
+                //NdefMessage readMessage =
+                command = new byte[1];
+                command[0] = (byte) 0x60; // GET_VERSION
+                answer = mfu.transceive(command);
+                Log.d(TAG, "answer: " + answer);
+                mfu.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+        //setIntent(intent);// was in original codexpedia code...need to figure out what it does. //todo
+        //This might be useful: http://www.codexpedia.com/android/android-nfc-read-and-write-example/
+        // this too: https://www.survivingwithandroid.com/2016/01/nfc-tag-writer-android.html
+
+
+
+
+
+        // next chunk mostly from here: https://www.survivingwithandroid.com/2015/03/android-nfc-app-android-nfc-tutorial.html
+
 
 
 
@@ -220,23 +263,16 @@ public class MainActivity extends Activity {
      **********************************Read From NFC Tag***************************
      ******************************************************************************/
     private void readFromIntent(Intent intent) {
-        String action = intent.getAction();
-        Log.d(TAG, "readFromIntent, action: " + action);
-        if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(action)
-                || NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)
-                || NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
-            Log.d(TAG, "matches one of the specified actions");
-            Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
-            NdefMessage[] msgs = null;
-            Log.d(TAG, "rawMsgs: " + rawMsgs);
-            if (rawMsgs != null) {
-                msgs = new NdefMessage[rawMsgs.length];
-                for (int i = 0; i < rawMsgs.length; i++) {
-                    msgs[i] = (NdefMessage) rawMsgs[i];
-                }
+        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        NdefMessage[] msgs = null;
+        Log.d(TAG, "rawMsgs: " + rawMsgs);
+        if (rawMsgs != null) {
+            msgs = new NdefMessage[rawMsgs.length];
+            for (int i = 0; i < rawMsgs.length; i++) {
+                msgs[i] = (NdefMessage) rawMsgs[i];
             }
-            buildTagViews(msgs, "Previous NFC Content: ", tvNFCContent);
         }
+        buildTagViews(msgs, "Just read NFC Content: ", justRead);
     }
 
     private void buildTagViews(NdefMessage[] msgs, String text, TextView tv) {
